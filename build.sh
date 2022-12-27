dadeb6ef4384551f934d08588ebeecbcc57981f9
Varshithvhegde <varshithvh@gmail.com>
2022-12-27 17:19:34 +0530

    new

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-pip install PyAudio-0.2.11-cp37-cp37m-win32.whl
+pip install PyAudio-0.2.11-cp39-cp39-win_amd64.whl
 pip install --upgrade pip
 pip install -r requirements.txt
